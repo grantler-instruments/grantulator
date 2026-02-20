0e982518b3e57aac5199f8f02659cbe7bea562a2
Thomas Geissl <thomas.geissl@gmail.com>
2026-02-20 20:22:54 +0100

    added gh action

--- grantulator/vite.config.ts
+++ grantulator/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/grantulator/',
   plugins: [react()],
   resolve: {
     dedupe: ['@emotion/styled', '@emotion/react', '@mui/material', '@mui/styled-engine'],
